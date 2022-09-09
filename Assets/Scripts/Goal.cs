41e2e3f6b7ea3bf84b341bc3c56573b5a1bea192
Max <renmaks@duck.com>
2022-09-09 13:42:55 +0500

    Создал репозиторий

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    // Статическое поле, доступное любому другому коду
+    static public bool goalMet = false;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Когда в область действия триггера попадает что-то, проверить, является ли это снарядом
+        if (other.gameObject.tag == "Projectile")
+        {
+            // Если это снаряд, присвоить полю goalMet значение true
+            Goal.goalMet = true;
+            // Также изменить альфа-канал цвета, чтобы увеличить непрозрачность
+            Material mat = GetComponent<Renderer>().material;
+            Color c = mat.color;
+            c.a = 1;
+            mat.color = c;
+        }
+    }
+
+}
