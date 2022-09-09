fa57aa3c595346898999785639bc4113f70eff36
Max <renmaks@duck.com>
2022-09-09 13:50:56 +0500

    Отредактировал скрипт Cloud

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -1,68 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Cloud : MonoBehaviour
 {
     [Header("Set In Inspector")]
-    public GameObject cloudSphere;
-    public int numSpheresMin = 6;
-    public int numSpheresMax = 10;
-    public Vector3 sphereOffsetScale = new Vector3(5, 2, 1);
-    public Vector2 sphereScaleRangeX = new Vector2(4, 8);
-    public Vector2 sphereScaleRangeY = new Vector2(3, 4);
-    public Vector2 sphereScaleRangeZ = new Vector2(2, 4);
-    public float scaleYMin = 2f;
+    [SerializeField] private GameObject cloudSphere;
 
-    private List<GameObject> spheres;
+    private const int _numSpheresMin = 6;
+    private const int _numSpheresMax = 10;
+    private readonly Vector3 _sphereOffsetScale = new Vector3(5, 2, 1);
+    private readonly Vector2 _sphereScaleRangeX = new Vector2(4, 8);
+    private readonly Vector2 _sphereScaleRangeY = new Vector2(3, 4);
+    private readonly Vector2 _sphereScaleRangeZ = new Vector2(2, 4);
+    private const float _scaleYMin = 2f;
+    private List<GameObject> _spheres;
 
 
-    void Start()
+    private void Start()
     {
-        spheres = new List<GameObject>();
+        _spheres = new List<GameObject>();
 
-        int num = Random.Range(numSpheresMin, numSpheresMax);
+        int num = Random.Range(_numSpheresMin, _numSpheresMax);
         for(int i = 0; i < num; i++)
         {
-            GameObject sp = Instantiate<GameObject>(cloudSphere);
-            spheres.Add(sp);
+            var sp = Instantiate<GameObject>(cloudSphere);
+            _spheres.Add(sp);
             Transform spTrans = sp.transform;
             spTrans.SetParent(this.transform);
 
             // Выбрать случайное местоположение
             Vector3 offset = Random.insideUnitSphere;
-            offset.x *= sphereOffsetScale.x;
-            offset.y *= sphereOffsetScale.y;
-            offset.z *= sphereOffsetScale.z;
+            offset.x *= _sphereOffsetScale.x;
+            offset.y *= _sphereOffsetScale.y;
+            offset.z *= _sphereOffsetScale.z;
             spTrans.localPosition = offset;
 
             // Выбрать случайный масштаб
             Vector3 scale = Vector3.one;
-            scale.x = Random.Range(sphereScaleRangeX.x, sphereScaleRangeX.y);
-            scale.y = Random.Range(sphereScaleRangeY.x, sphereScaleRangeY.y);
-            scale.z = Random.Range(sphereScaleRangeZ.x, sphereScaleRangeZ.y);
+            scale.x = Random.Range(_sphereScaleRangeX.x, _sphereScaleRangeX.y);
+            scale.y = Random.Range(_sphereScaleRangeY.x, _sphereScaleRangeY.y);
+            scale.z = Random.Range(_sphereScaleRangeZ.x, _sphereScaleRangeZ.y);
 
             // Скорректировать масштаб y по расстоянию x от центра
-            scale.y *= 1 - (Mathf.Abs(offset.x) / sphereOffsetScale.x);
-            scale.y = Mathf.Max(scale.y, scaleYMin);
+            scale.y *= 1 - (Mathf.Abs(offset.x) / _sphereOffsetScale.x);
+            scale.y = Mathf.Max(scale.y, _scaleYMin);
 
             spTrans.localScale = scale;
         }
-    }
-
-    void Update()
-    {
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //    Restart();
-    }
-
-    void Restart()
-    {
-        // Удалить старые сферы, составляющие облако
-        foreach(GameObject sp in spheres)
-        {
-            Destroy(sp);
-        }
-        Start();
     }
 }
